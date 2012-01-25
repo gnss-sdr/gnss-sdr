22925909fddd723165116d536f761cc0583ef5f0
Javier Arribas <javiarribas@gmail.com>
2012-01-25 16:23:54 +0000

    Added new classes Gnss_Synchro and Gnss_Signal
    git-svn-id: https://svn.code.sf.net/p/gnss-sdr/code/trunk@145 64b25241-fba3-4117-9849-534c7e92360d

---
+++ src/core/system_parameters/gnss_signal.h
@@ -0,0 +1,56 @@
+/*!
+ * \file gnss_satellite.cc
+ * \brief  Implementation of the Gnss_Signal class
+ * \author
+ *  Luis Esteve, 2012. luis(at)epsilon-formacion.com
+ *  Javier Arribas, 2012. jarribas(at)cttc.es
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2012  (see AUTHORS file for a list of contributors)
+ *
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ *
+ * GNSS-SDR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNSS-SDR. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+
+#ifndef GNSS_SDR_GNSS_SIGNAL_H_
+#define GNSS_SDR_GNSS_SIGNAL_H_
+
+#include "gnss_satellite.h"
+#include <string>
+/*
+ * \brief This class represents a GNSS signal.
+ *
+ * It contains information about the space vehicle and the specific signal.
+ */
+class Gnss_Signal
+{
+
+private:
+	Gnss_Satellite satellite;
+	std::string signal;
+public:
+	Gnss_Signal();
+	Gnss_Signal(Gnss_Satellite satellite_,std::string signal_);
+	~Gnss_Signal();
+	std::string get_signal();
+	Gnss_Satellite get_satellite();
+
+};
+#endif
---
+++ src/core/system_parameters/gnss_synchro.h
@@ -0,0 +1,74 @@
+/*!
+ * \file gnss_synchro.h
+ * \brief  Implementation of the Gnss_Synchro class
+ * \author
+ *  Luis Esteve, 2012. luis(at)epsilon-formacion.com
+ *  Javier Arribas, 2012. jarribas(at)cttc.es
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2012  (see AUTHORS file for a list of contributors)
+ *
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ *
+ * GNSS-SDR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNSS-SDR. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+#ifndef GNSS_SDR_GNSS_SYNCHRO_H_
+#define GNSS_SDR_GNSS_SYNCHRO_H_
+
+#include "gnss_signal.h"
+
+#include <string>
+
+class Gnss_Synchro{
+private:
+
+public:
+	Gnss_Synchro();
+	~Gnss_Synchro();
+
+	Gnss_Signal Signal;
+	// Acquisition
+	double Acq_delay_samples;
+	double Acq_doppler_hz;
+	unsigned long int Acq_samplestamp_samples;
+	bool Flag_valid_acquisition;
+	//Tracking
+	double Prompt_I;
+	double Prompt_Q;
+	double Carrier_phase_rads;
+	double Code_phase_secs;
+	double Tracking_timestamp_secs;
+	double CN0_dB_hz;
+	bool Flag_valid_tracking;
+	//Telemetry Decoder
+	double Preamble_delay_ms;
+	double Prn_delay_ms;
+	double Preamble_code_phase_ms;
+	double Preamble_code_phase_correction_ms;
+	int Channel_ID;
+	bool Flag_valid_word;
+	bool Flag_preamble;
+	// Pseudorange
+	double Pseudorange_m;
+	double Pseudorange_timestamp_ms;
+	bool Flag_valid_pseudorange;
+
+};
+#endif
+

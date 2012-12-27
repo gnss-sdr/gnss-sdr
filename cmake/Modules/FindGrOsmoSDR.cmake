b52846e7fd1d4a6d776555c58f4444d508851c0f
Carles Fernandez <carles.fernandez@gmail.com>
2012-12-27 17:09:16 +0000

    Added support for RTL devices. If the variable RTLSDR_DRIVER is set, CMake looks for libosmosdr and gnuradio-osmosdr libraries and compiles the corresponding signal source.
    git-svn-id: https://svn.code.sf.net/p/gnss-sdr/code/trunk@298 64b25241-fba3-4117-9849-534c7e92360d

---
+++ cmake/Modules/FindGrOsmoSDR.cmake
@@ -0,0 +1,51 @@
+# Tries to find gr-osmosdr.
+#
+# Usage of this module as follows:
+#
+# find_package(GrOsmoSDR)
+#
+# Variables used by this module, they can change the default behaviour and need
+# to be set before calling find_package:
+#
+# GrOsmoSDR_ROOT_DIR Set this variable to the root installation of
+# gr-osmosdr if the module has problems finding
+# the proper installation path.
+#
+# Variables defined by this module:
+#
+# GROSMOSDR_FOUND System has gr-osmosdr libs/headers
+# GROSMOSDR_LIBRARIES The gr-osmosdr libraries (gnuradio-osmosdr)
+# GROSMOSDR_INCLUDE_DIR The location of gr-osmosdr headers
+
+
+
+FIND_PATH(GROSMOSDR_INCLUDE_DIR 
+     NAMES osmosdr/osmosdr_source_c.h 
+           osmosdr/osmosdr_api.h
+     HINTS ${GrOsmoSDR_ROOT_DIR}/include
+     PATHS /usr/local/include
+           /usr/include
+)
+
+
+find_library(GROSMOSDR_LIBRARIES
+  NAMES gnuradio-osmosdr
+  HINTS ${GrOsmoSDR_ROOT_DIR}/lib
+  PATHS /usr/local/lib
+        /usr/lib
+  )
+
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  GrOsmoSDR
+  DEFAULT_MSG
+  GROSMOSDR_LIBRARIES
+  GROSMOSDR_INCLUDE_DIR
+)
+
+mark_as_advanced(
+  GrOsmoSDR_ROOT_DIR
+  GROSMOSDR_LIBRARIES
+  GROSMOSDR_INCLUDE_DIR
+)

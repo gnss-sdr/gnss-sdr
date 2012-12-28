a032dfaefd4e57c8ce52666a050bb5d0e1b513b4
Carles Fernandez <carles.fernandez@gmail.com>
2012-12-28 15:40:03 +0000

    Enabling CTest
    git-svn-id: https://svn.code.sf.net/p/gnss-sdr/code/trunk@301 64b25241-fba3-4117-9849-534c7e92360d

---
+++ src/tests/single_test_main.cc
@@ -0,0 +1,53 @@
+/*!
+ * \file single_test_main.cc
+ * \brief  This file contains the main function for tests (used with CTest).
+ * \author Carles Fernandez-Prades, 2012. cfernandez(at)cttc.es
+ *
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2013  (see AUTHORS file for a list of contributors)
+ *
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ *
+ * GNSS-SDR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNSS-SDR. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+
+#include <iostream>
+#include <queue>
+#include <gtest/gtest.h>
+#include <glog/log_severity.h>
+#include <glog/logging.h>
+#include <gflags/gflags.h>
+#include <gnuradio/gr_msg_queue.h>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/filesystem.hpp>
+#include "concurrent_queue.h"
+#include "gps_navigation_message.h"
+
+concurrent_queue<Gps_Navigation_Message> global_gps_nav_msg_queue;
+
+int main(int argc, char **argv)
+{
+	google::ParseCommandLineFlags(&argc, &argv, true);
+	testing::InitGoogleTest(&argc, argv);
+    google::InitGoogleLogging(argv[0]);
+    return RUN_ALL_TESTS();
+}

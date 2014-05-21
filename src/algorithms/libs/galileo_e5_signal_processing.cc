408fd992ec3cc0c3d255e6a109b27027f2d0d93f
marc-sales <marcsales92@gmail.com>
2014-05-21 09:42:26 +0200

    cabeceras

--- src/algorithms/libs/galileo_e5_signal_processing.cc
+++ src/algorithms/libs/galileo_e5_signal_processing.cc
@@ -1,8 +1,34 @@
-/*
- * galileo_e5_signal_processing.cc
+/*!
+ * \file galileo_e1_signal_processing.cc
+ * \brief This library implements various functions for Galileo E5 signals such
+ * as replica code generation
+ * \author Marc Sales, 2014. marcsales92(at)gmail.com
  *
- *  Created on: May 20, 2014
- *      Author: marc
+ * Detailed description of the file here if needed.
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2014  (see AUTHORS file for a list of contributors)
+ *
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ *
+ * GNSS-SDR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNSS-SDR. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
  */
 
 #include "galileo_e5_signal_processing.h"

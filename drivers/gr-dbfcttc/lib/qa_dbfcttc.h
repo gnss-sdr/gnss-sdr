5ab258d0e0d17a923837a9970e1670f7123fcbd8
Carles Fernandez <carles.fernandez@gmail.com>
2015-01-12 21:22:02 +0100

    Updating copyright year

--- drivers/gr-dbfcttc/lib/qa_dbfcttc.h
+++ drivers/gr-dbfcttc/lib/qa_dbfcttc.h
@@ -1,24 +1,28 @@
-/* -*- c++ -*- */
 /*
- * Copyright 2012 Free Software Foundation, Inc.
  *
- * This file is part of GNU Radio
+ * Copyright (C) 2010-2015  (see AUTHORS file for a list of contributors)
  *
- * GNU Radio is free software; you can redistribute it and/or modify
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * GNU Radio is distributed in the hope that it will be useful,
+ * GNSS-SDR is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GNU Radio; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street,
- * Boston, MA 02110-1301, USA.
+ * along with GNSS-SDR. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
  */
+
 
 #ifndef _QA_DBFCTTC_H_
 #define _QA_DBFCTTC_H_

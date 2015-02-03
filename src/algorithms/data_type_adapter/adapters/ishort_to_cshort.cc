f29c0c2467a3af3865e8b3926ec8d6688247f450
Carles Fernandez <carles.fernandez@gmail.com>
2015-02-03 19:41:47 +0100

    Adding missing files

---
+++ src/algorithms/data_type_adapter/adapters/ishort_to_cshort.cc
@@ -0,0 +1,113 @@
+/*!
+ * \file ishort_to_cshort.cc
+ * \brief Adapts an I/Q interleaved short integer (16 bits) sample stream
+ * to a gr_complex (float) stream
+ * \author Carles Fernandez-Prades, cfernandez(at)cttc.es
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2015  (see AUTHORS file for a list of contributors)
+ *
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GNSS-SDR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNSS-SDR. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+
+#include "ishort_to_cshort.h"
+#include <glog/logging.h>
+#include <volk/volk.h>
+#include "configuration_interface.h"
+
+using google::LogMessage;
+
+IshortToCshort::IshortToCshort(ConfigurationInterface* configuration, std::string role,
+        unsigned int in_streams, unsigned int out_streams,
+        boost::shared_ptr<gr::msg_queue> queue) :
+                config_(configuration), role_(role), in_streams_(in_streams),
+                out_streams_(out_streams), queue_(queue)
+{
+
+    std::string default_input_item_type = "short";
+    std::string default_output_item_type = "cshort";
+    std::string default_dump_filename = "../data/input_filter.dat";
+
+    DLOG(INFO) << "role " << role_;
+
+    input_item_type_ = config_->property(role_ + ".input_item_type",
+                                         default_input_item_type);
+
+    dump_ = config_->property(role_ + ".dump", false);
+    dump_filename_ = config_->property(role_ + ".dump_filename",
+                                       default_dump_filename);
+
+    size_t item_size = sizeof(lv_16sc_t);
+
+    interleaved_short_to_complex_short_ = make_interleaved_short_to_complex_short();
+
+    DLOG(INFO) << "data_type_adapter_(" << interleaved_short_to_complex_short_->unique_id() << ")";
+
+    if (dump_)
+        {
+            DLOG(INFO) << "Dumping output into file " << dump_filename_;
+            file_sink_ = gr::blocks::file_sink::make(item_size, dump_filename_.c_str());
+        }
+
+}
+
+
+IshortToCshort::~IshortToCshort()
+{}
+
+
+void IshortToCshort::connect(gr::top_block_sptr top_block)
+{
+    if (dump_)
+        {
+            top_block->connect(interleaved_short_to_complex_short_, 0, file_sink_, 0);
+        }
+    else
+        {
+            DLOG(INFO) << "Nothing to connect internally";
+        }
+}
+
+
+void IshortToCshort::disconnect(gr::top_block_sptr top_block)
+{
+    if (dump_)
+        {
+            top_block->disconnect(interleaved_short_to_complex_short_, 0, file_sink_, 0);
+        }
+}
+
+
+
+gr::basic_block_sptr IshortToCshort::get_left_block()
+{
+    return interleaved_short_to_complex_short_;
+}
+
+
+
+gr::basic_block_sptr IshortToCshort::get_right_block()
+{
+    return interleaved_short_to_complex_short_;
+}
+
+
---
+++ src/algorithms/data_type_adapter/adapters/ishort_to_cshort.h
@@ -0,0 +1,92 @@
+/*!
+ * \file ishort_to_cshort.h
+ * \brief Adapts a short interleaved sample stream into a std::complex<short> stream
+ * \author Carles Fernandez-Prades, cfernandez(at)cttc.es
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2015  (see AUTHORS file for a list of contributors)
+ *
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GNSS-SDR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNSS-SDR. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+
+#ifndef GNSS_SDR_ISHORT_TO_CSHORT_H_
+#define GNSS_SDR_ISHORT_TO_CSHORT_H_
+
+#include <string>
+#include <gnuradio/blocks/file_sink.h>
+#include <gnuradio/msg_queue.h>
+#include "gnss_synchro.h"
+#include "gnss_block_interface.h"
+#include "interleaved_short_to_complex_short.h"
+
+
+
+class ConfigurationInterface;
+
+/*!
+ * \brief Adapts a short integer (16 bits) interleaved sample stream into a std::complex<short> stream
+ *
+ */
+class IshortToCshort: public GNSSBlockInterface
+{
+public:
+    IshortToCshort(ConfigurationInterface* configuration,
+            std::string role, unsigned int in_streams,
+            unsigned int out_streams, boost::shared_ptr<gr::msg_queue> queue);
+
+    virtual ~IshortToCshort();
+
+    std::string role()
+    {
+        return role_;
+    }
+    //! Returns "IshortToCshort"
+    std::string implementation()
+    {
+        return "IshortToCshort";
+    }
+    size_t item_size()
+    {
+        return 0;
+    }
+
+    void connect(gr::top_block_sptr top_block);
+    void disconnect(gr::top_block_sptr top_block);
+    gr::basic_block_sptr get_left_block();
+    gr::basic_block_sptr get_right_block();
+
+private:
+    interleaved_short_to_complex_short_sptr interleaved_short_to_complex_short_;
+    ConfigurationInterface* config_;
+    bool dump_;
+    std::string dump_filename_;
+    std::string input_item_type_;
+    std::string output_item_type_;
+    std::string role_;
+    unsigned int in_streams_;
+    unsigned int out_streams_;
+    boost::shared_ptr<gr::msg_queue> queue_;
+    gr::blocks::file_sink::sptr file_sink_;
+};
+
+#endif
+

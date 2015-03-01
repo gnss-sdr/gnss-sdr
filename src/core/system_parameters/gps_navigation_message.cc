1fac79845418e33ca0486ee5e47c2f83f6c20b9a
Carles Fernandez <carles.fernandez@gmail.com>
2015-03-01 19:53:28 +0100

    Remove warning

--- src/core/system_parameters/gps_navigation_message.cc
+++ src/core/system_parameters/gps_navigation_message.cc
@@ -634,7 +634,7 @@ int Gps_Navigation_Message::subframe_decoder(char *subframe)
         if (SV_page_5 < 25)
             {
                 //! \TODO read almanac
-                if(SV_data_ID){}
+                if(SV_data_ID_5){}
             }
         if (SV_page_5 == 51) // Page 25 (from Table 20-V. Data IDs and SV IDs in Subframes 4 and 5, IS-GPS-200H, page 110)
             {

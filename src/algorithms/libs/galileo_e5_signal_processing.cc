644af36a5e2c10ad47ffabafdfad434fd9ab54b3
Carles Fernandez <carles.fernandez@gmail.com>
2015-05-21 20:27:39 +0200

    fixing initialization

--- src/algorithms/libs/galileo_e5_signal_processing.cc
+++ src/algorithms/libs/galileo_e5_signal_processing.cc
@@ -104,8 +104,7 @@ void galileo_e5_a_code_gen_complex_sampled(std::complex<float>* _dest, char _Sig
     unsigned int _codeLength = Galileo_E5a_CODE_LENGTH_CHIPS;
     const int _codeFreqBasis = Galileo_E5a_CODE_CHIP_RATE_HZ; //Hz
 
-    std::complex<float>* _code;
-    _code = new std::complex<float>[_codeLength];
+    std::complex<float>* _code = new std::complex<float>[_codeLength]();
 
     galileo_e5_a_code_gen_complex_primary(_code , _prn , _Signal);
 

882e3e4ffed47dfb196838881e2ead1d1351ce5a
Cillian O'Driscoll <cillian.odriscoll@gmail.com>
2016-09-01 11:48:58 +0100

    Final fix for volk_32f_index_max
    I hope...

--- cmake/Modules/FindVolk.cmake
+++ cmake/Modules/FindVolk.cmake
@@ -9,25 +9,25 @@ FIND_PATH(
     VOLK_INCLUDE_DIRS
     NAMES volk/volk.h
     HINTS $ENV{VOLK_DIR}/include
-        ${PC_VOLK_INCLUDEDIR}
+          ${PC_VOLK_INCLUDEDIR}
     PATHS /usr/local/include
           /usr/include
-          ${GNURADIO_INSTALL_PREFIX}/include
+          ${CMAKE_INSTALL_PREFIX}/include
 )
 
 FIND_LIBRARY(
     VOLK_LIBRARIES
     NAMES volk
     HINTS $ENV{VOLK_DIR}/lib
-        ${PC_VOLK_LIBDIR}
+          ${PC_VOLK_LIBDIR}
     PATHS /usr/local/lib
           /usr/local/lib64
           /usr/lib
           /usr/lib64
-          ${GNURADIO_INSTALL_PREFIX}/lib
+          ${CMAKE_INSTALL_PREFIX}/lib
 )
 
-set(VOLK_VERSION ${PC_VOLK_VERSION})
+#set(VOLK_VERSION ${PC_VOLK_VERSION})
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(VOLK DEFAULT_MSG VOLK_LIBRARIES VOLK_INCLUDE_DIRS)

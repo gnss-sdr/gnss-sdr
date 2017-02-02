59e3ffe1674721a231195f219e5f484ead537470
Carles Fernandez <carles.fernandez@gmail.com>
2017-02-02 20:05:15 +0100

    Add header

--- src/tests/unit-tests/signal-processing-blocks/libs/tracking_dump_reader.cc
+++ src/tests/unit-tests/signal-processing-blocks/libs/tracking_dump_reader.cc
@@ -1,12 +1,38 @@
-//
-// Created by javier on 1/2/2017.
-//
+/*!
+ * \file tracking_dump_reader.cc
+ * \brief Helper file for unit testing
+ * \author Javier Arribas, 2017. jarribas(at)cttc.es
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2017  (see AUTHORS file for a list of contributors)
+ *
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GNSS-SDR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNSS-SDR. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
 
 #include "tracking_dump_reader.h"
 
 bool tracking_dump_reader::read_binary_obs()
-    {
-        try {
+{
+    try {
             d_dump_file.read((char *) &abs_E, sizeof(float));
             d_dump_file.read((char *) &abs_P, sizeof(float));
             d_dump_file.read((char *) &abs_L, sizeof(float));
@@ -27,22 +53,26 @@ bool tracking_dump_reader::read_binary_obs()
             d_dump_file.read((char *) &aux1, sizeof(double));
             d_dump_file.read((char *) &aux2, sizeof(double));
 
+    }
+    catch (const std::ifstream::failure &e)
+    {
+            return false;
+    }
+    return true;
+}
+
+bool tracking_dump_reader::restart()
+{
+    if (d_dump_file.is_open())
+        {
+            d_dump_file.clear();
+            d_dump_file.seekg(0, std::ios::beg);
+            return true;
         }
-        catch (const std::ifstream::failure &e) {
+    else
+        {
             return false;
         }
-        return true;
-    }
-
-bool tracking_dump_reader::restart() {
-    if (d_dump_file.is_open())
-    {
-        d_dump_file.clear();
-        d_dump_file.seekg(0, std::ios::beg);
-        return true;
-    }else{
-        return false;
-    }
 }
 
 long int tracking_dump_reader::num_epochs()
@@ -50,44 +80,48 @@ long int tracking_dump_reader::num_epochs()
     std::ifstream::pos_type size;
     int number_of_double_vars=11;
     int number_of_float_vars=5;
-    int epoch_size_bytes=sizeof(unsigned long int)+
-            sizeof(double)*number_of_double_vars+
-            sizeof(float)*number_of_float_vars;
+    int epoch_size_bytes=sizeof(unsigned long int) +
+            sizeof(double) * number_of_double_vars +
+            sizeof(float) * number_of_float_vars;
     std::ifstream tmpfile( d_dump_filename.c_str(), std::ios::binary | std::ios::ate);
     if (tmpfile.is_open())
         {
             size = tmpfile.tellg();
-            long int  nepoch=size / epoch_size_bytes;
+            long int nepoch = size / epoch_size_bytes;
             return nepoch;
-        }else{
+        }
+    else
+        {
             return 0;
         }
 }
 
-bool tracking_dump_reader::open_obs_file(std::string out_file) {
+bool tracking_dump_reader::open_obs_file(std::string out_file)
+{
     if (d_dump_file.is_open() == false)
-    {
-        try
         {
-            d_dump_filename=out_file;
-            d_dump_file.exceptions ( std::ifstream::failbit | std::ifstream::badbit );
-            d_dump_file.open(d_dump_filename.c_str(), std::ios::in | std::ios::binary);
-            std::cout << "Tracking dump enabled, Log file: " << d_dump_filename.c_str()<< std::endl;
-            return true;
+            try
+            {
+                    d_dump_filename = out_file;
+                    d_dump_file.exceptions ( std::ifstream::failbit | std::ifstream::badbit );
+                    d_dump_file.open(d_dump_filename.c_str(), std::ios::in | std::ios::binary);
+                    std::cout << "Tracking dump enabled, Log file: " << d_dump_filename.c_str() << std::endl;
+                    return true;
+            }
+            catch (const std::ifstream::failure & e)
+            {
+                    std::cout << "Problem opening Tracking dump Log file: " << d_dump_filename.c_str() << std::endl;
+                    return false;
+            }
+        }else{
+                return false;
         }
-        catch (const std::ifstream::failure & e)
-        {
-            std::cout << "Problem opening Tracking dump Log file: " << d_dump_filename.c_str()<< std::endl;
-            return false;
-        }
-    }else{
-        return false;
-    }
 }
 
-tracking_dump_reader::~tracking_dump_reader() {
+tracking_dump_reader::~tracking_dump_reader()
+{
     if (d_dump_file.is_open() == true)
-    {
-        d_dump_file.close();
-    }
+        {
+            d_dump_file.close();
+        }
 }

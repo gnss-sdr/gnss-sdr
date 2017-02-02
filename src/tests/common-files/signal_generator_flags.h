e078ebfd109409745346ce422b802912a118de3c
Carles Fernandez <carles.fernandez@gmail.com>
2017-02-02 20:37:46 +0100

    Add header

--- src/tests/common-files/signal_generator_flags.h
+++ src/tests/common-files/signal_generator_flags.h
@@ -1,3 +1,35 @@
+/*!
+ * \file signal_generator_flags.h
+ * \brief Helper file for unit testing
+ * \author Carles Fernandez-Prades, 2017. cfernandez(at)cttc.es
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2017  (see AUTHORS file for a list of contributors)
+ *
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GNSS-SDR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNSS-SDR. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+
+#ifndef GNSS_SDR_SIGNAL_GENERATOR_FLAGS_H_
+#define GNSS_SDR_SIGNAL_GENERATOR_FLAGS_H_
 
 #include <gflags/gflags.h>
 
@@ -11,3 +43,6 @@ DEFINE_string(filename_rinex_obs, "sim.16o", "Filename of output RINEX navigatio
 DEFINE_string(filename_raw_data, "signal_out.bin", "Filename of output raw data file");
 DEFINE_int32(fs_gen_hz, 2600000, "Samppling frequency [Hz]");
 DEFINE_int32(test_satellite_PRN, 1, "PRN of the satellite under test (must be visible during the observation time)");
+
+
+#endif

1cb4425cdea1c1c8145e5c3e32ab0af7b940377b
Carles Fernandez <carles.fernandez@gmail.com>
2017-05-06 10:45:33 +0200

    Fix PPP configuration

--- src/tests/system-tests/position_test.cc
+++ src/tests/system-tests/position_test.cc
@@ -266,9 +266,9 @@ int Position_Gps_L1_System_Test::configure_receiver()
     const float dll_bw_narrow_hz = 2.0;
     const int extend_correlation_ms = 1;
 
-    const int display_rate_ms = 500;
-    const int output_rate_ms = 10;
-    const int positioning_mode = 0;
+    const int display_rate_ms = 1000;
+    const int output_rate_ms = 1000;
+    const int averaging_depth = 1;
 
     config->set_property("GNSS-SDR.internal_fs_hz", std::to_string(sampling_rate_internal));
 
@@ -368,6 +368,9 @@ int Position_Gps_L1_System_Test::configure_receiver()
 
     // Set PVT
     config->set_property("PVT.implementation", "RTKLIB_PVT");
+    //config->set_property("PVT.implementation", "Hybrid_PVT");
+    config->set_property("PVT.averaging_depth", std::to_string(averaging_depth));
+    config->set_property("PVT.flag_averaging", "false");
     config->set_property("PVT.output_rate_ms", std::to_string(output_rate_ms));
     config->set_property("PVT.display_rate_ms", std::to_string(display_rate_ms));
     config->set_property("PVT.dump_filename", "./PVT");
@@ -379,9 +382,10 @@ int Position_Gps_L1_System_Test::configure_receiver()
     config->set_property("PVT.rtcm_dump_devname", "/dev/pts/1");
     config->set_property("PVT.dump", "false");
     config->set_property("PVT.rinex_version", std::to_string(2));
-    config->set_property("PVT.positioning_mode", std::to_string(positioning_mode));
+    config->set_property("PVT.positioning_mode", std::to_string(7));
     config->set_property("PVT.iono_model", std::to_string(0));
     config->set_property("PVT.trop_model", std::to_string(0));
+    config->set_property("PVT.AR_GPS", std::to_string(4));
     return 0;
 }
 

4807c55184734abc701d1a49c8be67463b08287f
Carles Fernandez <carles.fernandez@gmail.com>
2017-08-18 13:42:27 +0200

    Fix GCC warnings

--- src/tests/unit-tests/signal-processing-blocks/pvt/rtcm_test.cc
+++ src/tests/unit-tests/signal-processing-blocks/pvt/rtcm_test.cc
@@ -268,7 +268,7 @@ TEST(RtcmTest, MT1019)
     std::string tx_msg = rtcm->print_MT1019(gps_eph);
 
     EXPECT_EQ(0, rtcm->read_MT1019(tx_msg, gps_eph_read));
-    EXPECT_EQ(3, gps_eph_read.i_satellite_PRN);
+    EXPECT_EQ(static_cast<unsigned int>(3), gps_eph_read.i_satellite_PRN);
     EXPECT_DOUBLE_EQ(4, gps_eph_read.d_IODC);
     EXPECT_DOUBLE_EQ( 2.0 * E_LSB, gps_eph_read.d_e_eccentricity);
     EXPECT_EQ(expected_true, gps_eph_read.b_fit_interval_flag);
@@ -311,7 +311,7 @@ TEST(RtcmTest, MT1045)
     EXPECT_EQ(0, rtcm->read_MT1045(tx_msg, gal_eph_read));
     EXPECT_EQ(expected_true, gal_eph_read.E5a_DVS);
     EXPECT_DOUBLE_EQ( 53.0 * OMEGA_dot_3_LSB, gal_eph_read.OMEGA_dot_3);
-    EXPECT_EQ(5, gal_eph_read.i_satellite_PRN);
+    EXPECT_EQ(static_cast<unsigned int>(5), gal_eph_read.i_satellite_PRN);
     EXPECT_EQ(1, rtcm->read_MT1045(rtcm->bin_to_binary_data(rtcm->hex_to_bin("FFFFFFFFFFF")), gal_eph_read));
 }
 

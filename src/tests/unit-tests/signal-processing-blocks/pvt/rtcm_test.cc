2685dd0dfa453ec2e2aa3b651cb8f82634fabb32
Carles Fernandez <carles.fernandez@gmail.com>
2018-01-24 18:21:45 +0100

    Fix typo

--- src/tests/unit-tests/signal-processing-blocks/pvt/rtcm_test.cc
+++ src/tests/unit-tests/signal-processing-blocks/pvt/rtcm_test.cc
@@ -356,7 +356,7 @@ TEST(RtcmTest, MSMCell)
     auto rtcm = std::make_shared<Rtcm>();
     Gps_Ephemeris gps_eph = Gps_Ephemeris();
     Galileo_Ephemeris gal_eph = Galileo_Ephemeris();
-    // Glonass_Gnav_Ephemeris glo_gnav_eph = Glonass_Gnav_Ephemeris();
+    Glonass_Gnav_Ephemeris glo_gnav_eph = Glonass_Gnav_Ephemeris();
     std::map<int, Gnss_Synchro> pseudoranges;
 
     Gnss_Synchro gnss_synchro;

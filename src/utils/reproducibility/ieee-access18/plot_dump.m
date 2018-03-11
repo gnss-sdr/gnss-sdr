df1a3a13df7047f82854a37238c46bb3a99c60f0
Carles Fernandez <carles.fernandez@gmail.com>
2018-03-11 20:09:49 +0100

    Fix satellite

--- src/utils/reproducibility/ieee-access18/plot_dump.m
+++ src/utils/reproducibility/ieee-access18/plot_dump.m
@@ -227,11 +227,13 @@ hSurface=surf(MRSE*x,MRSE*y,MRSE*z);  % sphere centered at origin
 set(hSurface,'facecolor','none','edgecolor',[0 0.6 0],'edgealpha',1,'facealpha',1);
 
 %axis equal;
-xlabel('North [m]','fontname','Times','fontsize', fontsize)
-ylabel('East [m]','fontname','Times','fontsize', fontsize)
-zlabel('Up [m]','fontname','Times','fontsize', fontsize)
-str = strcat('MRSE=',num2str(MRSE), ' m');
+xlabel('North [m]','fontname','Times','fontsize', fontsize-2)
+ylabel('East [m]','fontname','Times','fontsize', fontsize-2)
+zlabel('Up [m]','fontname','Times','fontsize', fontsize-2)
+str = strcat('MRSE=',num2str(MRSE), ' m')
 text(cosd(45)*MRSE,sind(45)*MRSE,20,str,'Color',[0 0.6 0]);
+a=gca;
+set(a,'fontsize',fontsize-6)
 
 hh=findall(hf,'-property','FontName');
 set(hh,'FontName','Times');

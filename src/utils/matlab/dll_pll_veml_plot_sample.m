aa1e98f943d52cd66bad123b94ead00c2f59a15b
Carles Fernandez <carles.fernandez@gmail.com>
2018-03-30 12:04:14 +0200

    Get rid of ^M character

--- src/utils/matlab/dll_pll_veml_plot_sample.m
+++ src/utils/matlab/dll_pll_veml_plot_sample.m
@@ -10,7 +10,7 @@
 %           Satellite Systems receiver
 %
 % This file is part of GNSS-SDR.
-% 
+%
 % GNSS-SDR is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
@@ -56,10 +56,10 @@ for N=1:1:channels
     trackResults(N).dllDiscrFilt   = GNSS_tracking(N).code_nco.';
     trackResults(N).pllDiscr       = GNSS_tracking(N).carr_error.';
     trackResults(N).pllDiscrFilt   = GNSS_tracking(N).carr_nco.';
-    
+
     trackResults(N).I_P = GNSS_tracking(N).P.';
     trackResults(N).Q_P = zeros(1,length(GNSS_tracking(N).P));
-    
+
     trackResults(N).I_VE = GNSS_tracking(N).VE.';
     trackResults(N).I_E = GNSS_tracking(N).E.';
     trackResults(N).I_L = GNSS_tracking(N).L.';
@@ -72,7 +72,7 @@ for N=1:1:channels
     trackResults(N).data_Q = GNSS_tracking(N).prompt_Q.';
     trackResults(N).PRN = GNSS_tracking(N).PRN.';
     trackResults(N).CNo = GNSS_tracking(N).CN0_SNV_dB_Hz.';
-    
+
     % Use original MATLAB tracking plot function
     settings.numberOfChannels = channels;
     settings.msToProcess = length(GNSS_tracking(N).E) * coherent_integration_time_ms;

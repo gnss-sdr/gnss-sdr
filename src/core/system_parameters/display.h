7bb4b0e065e09bc20d7714a64453318df77b9d0d
Carles Fernandez <carles.fernandez@gmail.com>
2018-04-03 23:22:20 +0200

    Colors for everyone

--- src/core/system_parameters/display.h
+++ src/core/system_parameters/display.h
@@ -31,47 +31,52 @@
 #ifndef GNSS_SDR_DISPLAY_H_
 #define GNSS_SDR_DISPLAY_H_
 
-#include<string>
+#include <string>
+
+#ifndef NO_DISPLAY_COLORS
+#define DISPLAY_COLORS 1
+#endif
+
 
 #ifdef DISPLAY_COLORS
 
-const std::string TEXT_RESET        = "\033[0m";
-const std::string TEXT_BLACK        = "\033[30m";
-const std::string TEXT_RED          = "\033[31m";
-const std::string TEXT_GREEN        = "\033[32m";
-const std::string TEXT_YELLOW       = "\033[33m";
-const std::string TEXT_BLUE         = "\033[34m";
-const std::string TEXT_MAGENTA      = "\033[35m";
-const std::string TEXT_CYAN         = "\033[36m";
-const std::string TEXT_WHITE        = "\033[37m";
-const std::string TEXT_BOLD_BLACK   = "\033[1m\033[30m";
-const std::string TEXT_BOLD_RED     = "\033[1m\033[31m";
-const std::string TEXT_BOLD_GREEN   = "\033[1m\033[32m";
-const std::string TEXT_BOLD_YELLOW  = "\033[1m\033[33m";
-const std::string TEXT_BOLD_BLUE    = "\033[1m\033[34m";
+const std::string TEXT_RESET = "\033[0m";
+const std::string TEXT_BLACK = "\033[30m";
+const std::string TEXT_RED = "\033[31m";
+const std::string TEXT_GREEN = "\033[32m";
+const std::string TEXT_YELLOW = "\033[33m";
+const std::string TEXT_BLUE = "\033[34m";
+const std::string TEXT_MAGENTA = "\033[35m";
+const std::string TEXT_CYAN = "\033[36m";
+const std::string TEXT_WHITE = "\033[37m";
+const std::string TEXT_BOLD_BLACK = "\033[1m\033[30m";
+const std::string TEXT_BOLD_RED = "\033[1m\033[31m";
+const std::string TEXT_BOLD_GREEN = "\033[1m\033[32m";
+const std::string TEXT_BOLD_YELLOW = "\033[1m\033[33m";
+const std::string TEXT_BOLD_BLUE = "\033[1m\033[34m";
 const std::string TEXT_BOLD_MAGENTA = "\033[1m\033[35m";
-const std::string TEXT_BOLD_CYAN    = "\033[1m\033[36m";
-const std::string TEXT_BOLD_WHITE   = "\033[1m\033[37m";
+const std::string TEXT_BOLD_CYAN = "\033[1m\033[36m";
+const std::string TEXT_BOLD_WHITE = "\033[1m\033[37m";
 
 #else
 
-const std::string TEXT_RESET        = "";
-const std::string TEXT_BLACK        = "";
-const std::string TEXT_RED          = "";
-const std::string TEXT_GREEN        = "";
-const std::string TEXT_YELLOW       = "";
-const std::string TEXT_BLUE         = "";
-const std::string TEXT_MAGENTA      = "";
-const std::string TEXT_CYAN         = "";
-const std::string TEXT_WHITE        = "";
-const std::string TEXT_BOLD_BLACK   = "";
-const std::string TEXT_BOLD_RED     = "";
-const std::string TEXT_BOLD_GREEN   = "";
-const std::string TEXT_BOLD_YELLOW  = "";
-const std::string TEXT_BOLD_BLUE    = "";
+const std::string TEXT_RESET = "";
+const std::string TEXT_BLACK = "";
+const std::string TEXT_RED = "";
+const std::string TEXT_GREEN = "";
+const std::string TEXT_YELLOW = "";
+const std::string TEXT_BLUE = "";
+const std::string TEXT_MAGENTA = "";
+const std::string TEXT_CYAN = "";
+const std::string TEXT_WHITE = "";
+const std::string TEXT_BOLD_BLACK = "";
+const std::string TEXT_BOLD_RED = "";
+const std::string TEXT_BOLD_GREEN = "";
+const std::string TEXT_BOLD_YELLOW = "";
+const std::string TEXT_BOLD_BLUE = "";
 const std::string TEXT_BOLD_MAGENTA = "";
-const std::string TEXT_BOLD_CYAN    = "";
-const std::string TEXT_BOLD_WHITE   = "";
+const std::string TEXT_BOLD_CYAN = "";
+const std::string TEXT_BOLD_WHITE = "";
 
 #endif /* DISPLAY_COLORS      */
 #endif /* GNSS_SDR_DISPLAY_H_ */

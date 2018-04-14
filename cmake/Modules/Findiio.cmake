b1695375a854bda08df9c2f509b3af574171417a
Carles Fernandez <carles.fernandez@gmail.com>
2018-04-14 16:04:33 +0200

    Improve finding of gnuradio-iio library

--- cmake/Modules/Findiio.cmake
+++ cmake/Modules/Findiio.cmake
@@ -22,6 +22,28 @@ FIND_LIBRARY(
           /usr/local/lib64
           /usr/lib
           /usr/lib64
+          /usr/lib/x86_64-linux-gnu
+          /usr/lib/gcc/alpha-linux-gnu
+          /usr/lib/gcc/aarch64-linux-gnu
+          /usr/lib/gcc/arm-linux-gnueabi
+          /usr/lib/gcc/arm-linux-gnueabihf
+          /usr/lib/gcc/hppa-linux-gnu
+          /usr/lib/gcc/i686-gnu
+          /usr/lib/gcc/i686-linux-gnu
+          /usr/lib/gcc/x86_64-kfreebsd-gnu
+          /usr/lib/gcc/i686-kfreebsd-gnu
+          /usr/lib/gcc/m68k-linux-gnu
+          /usr/lib/gcc/mips-linux-gnu
+          /usr/lib/gcc/mips64el-linux-gnuabi64
+          /usr/lib/gcc/mipsel-linux-gnu
+          /usr/lib/gcc/powerpc-linux-gnu
+          /usr/lib/gcc/powerpc-linux-gnuspe
+          /usr/lib/gcc/powerpc64-linux-gnu
+          /usr/lib/gcc/powerpc64le-linux-gnu
+          /usr/lib/gcc/s390x-linux-gnu
+          /usr/lib/gcc/sparc64-linux-gnu
+          /usr/lib/gcc/x86_64-linux-gnux32
+          /usr/lib/gcc/sh4-linux-gnu
 )
 
 INCLUDE(FindPackageHandleStandardArgs)

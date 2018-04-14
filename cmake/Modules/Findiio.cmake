bce730f68da4c93d05a26aceed409e23d8acedb1
Carles Fernandez <carles.fernandez@gmail.com>
2018-04-14 16:12:21 +0200

    Improve finding of gnuradio-iio and libiio libraries

--- cmake/Modules/Findiio.cmake
+++ cmake/Modules/Findiio.cmake
@@ -23,27 +23,27 @@ FIND_LIBRARY(
           /usr/lib
           /usr/lib64
           /usr/lib/x86_64-linux-gnu
-          /usr/lib/gcc/alpha-linux-gnu
-          /usr/lib/gcc/aarch64-linux-gnu
-          /usr/lib/gcc/arm-linux-gnueabi
-          /usr/lib/gcc/arm-linux-gnueabihf
-          /usr/lib/gcc/hppa-linux-gnu
-          /usr/lib/gcc/i686-gnu
-          /usr/lib/gcc/i686-linux-gnu
-          /usr/lib/gcc/x86_64-kfreebsd-gnu
-          /usr/lib/gcc/i686-kfreebsd-gnu
-          /usr/lib/gcc/m68k-linux-gnu
-          /usr/lib/gcc/mips-linux-gnu
-          /usr/lib/gcc/mips64el-linux-gnuabi64
-          /usr/lib/gcc/mipsel-linux-gnu
-          /usr/lib/gcc/powerpc-linux-gnu
-          /usr/lib/gcc/powerpc-linux-gnuspe
-          /usr/lib/gcc/powerpc64-linux-gnu
-          /usr/lib/gcc/powerpc64le-linux-gnu
-          /usr/lib/gcc/s390x-linux-gnu
-          /usr/lib/gcc/sparc64-linux-gnu
-          /usr/lib/gcc/x86_64-linux-gnux32
-          /usr/lib/gcc/sh4-linux-gnu
+          /usr/lib/alpha-linux-gnu
+          /usr/lib/aarch64-linux-gnu
+          /usr/lib/arm-linux-gnueabi
+          /usr/lib/arm-linux-gnueabihf
+          /usr/lib/hppa-linux-gnu
+          /usr/lib/i686-gnu
+          /usr/lib/i686-linux-gnu
+          /usr/lib/x86_64-kfreebsd-gnu
+          /usr/lib/i686-kfreebsd-gnu
+          /usr/lib/m68k-linux-gnu
+          /usr/lib/mips-linux-gnu
+          /usr/lib/mips64el-linux-gnuabi64
+          /usr/lib/mipsel-linux-gnu
+          /usr/lib/powerpc-linux-gnu
+          /usr/lib/powerpc-linux-gnuspe
+          /usr/lib/powerpc64-linux-gnu
+          /usr/lib/powerpc64le-linux-gnu
+          /usr/lib/s390x-linux-gnu
+          /usr/lib/sparc64-linux-gnu
+          /usr/lib/x86_64-linux-gnux32
+          /usr/lib/sh4-linux-gnu
 )
 
 INCLUDE(FindPackageHandleStandardArgs)

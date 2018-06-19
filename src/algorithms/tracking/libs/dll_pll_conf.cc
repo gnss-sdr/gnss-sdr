acd662b88fbe8caddbb0d88925cfaadda46acf3b
Carles Fernandez <carles.fernandez@gmail.com>
2018-06-19 12:57:43 +0200

    Add file header

--- src/algorithms/tracking/libs/dll_pll_conf.cc
+++ src/algorithms/tracking/libs/dll_pll_conf.cc
@@ -1,3 +1,35 @@
+/*!
+ * \file dll_pll_conf.cc
+ * \brief Class that contains all the configuration parameters for generic
+ * tracking block based on a DLL and a PLL.
+ * \author Javier Arribas, 2018. jarribas(at)cttc.es
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2018  (see AUTHORS file for a list of contributors)
+ *
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GNSS-SDR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNSS-SDR. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+
+
 #include "dll_pll_conf.h"
 #include <cstring>
 

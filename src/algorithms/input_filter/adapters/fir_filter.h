0f6a5d16ac2f0fdfbde6873a78e70b9022e49d56
Carles Fernandez <carles.fernandez@gmail.com>
2018-08-31 15:51:07 +0200

    Fix build with latest GNU Radio master (towards 3.8)

--- src/algorithms/input_filter/adapters/fir_filter.h
+++ src/algorithms/input_filter/adapters/fir_filter.h
@@ -44,7 +44,7 @@
 #include <gnuradio/blocks/float_to_complex.h>
 #include <gnuradio/blocks/float_to_short.h>
 #ifdef GR_GREATER_38
-#include <gnuradio/filter/fir_filter.h>
+#include <gnuradio/filter/fir_filter_blk.h>
 #else
 #include <gnuradio/filter/fir_filter_ccf.h>
 #include <gnuradio/filter/fir_filter_fff.h>

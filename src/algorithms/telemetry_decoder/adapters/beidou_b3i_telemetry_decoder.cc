5a4227507de9b3592cfc5858ab99c27b7460fa4f
Damian Miralles <dmiralles2009@gmail.com>
2019-03-17 18:57:45 -0500

    bds_b3i: fixing code with clang format

--- src/algorithms/telemetry_decoder/adapters/beidou_b3i_telemetry_decoder.cc
+++ src/algorithms/telemetry_decoder/adapters/beidou_b3i_telemetry_decoder.cc
@@ -1,6 +1,6 @@
 /*!
  * \file beidou_b3i_telemetry_decoder.cc
- * \brief Implementation of an adapter of a Beidou B1I NAV data decoder block
+ * \brief Implementation of an adapter of a Beidou B3I NAV data decoder block
  * to a TelemetryDecoderInterface
  * \author Damian Miralles, 2019. dmiralles2009@gmail.com
  *
@@ -29,78 +29,56 @@
  * -------------------------------------------------------------------------
  */
 
-
 #include "beidou_b3i_telemetry_decoder.h"
 #include "configuration_interface.h"
-#include <gnuradio/io_signature.h>
 #include <glog/logging.h>
 
-
-BeidouB3iTelemetryDecoder::BeidouB3iTelemetryDecoder(ConfigurationInterface* configuration,
-    std::string role,
-    unsigned int in_streams,
-    unsigned int out_streams) : role_(role),
-                                in_streams_(in_streams),
-                                out_streams_(out_streams)
-{
-    std::string default_dump_filename = "./navigation.dat";
-    DLOG(INFO) << "role " << role;
-    dump_ = configuration->property(role + ".dump", false);
-    dump_filename_ = configuration->property(role + ".dump_filename", default_dump_filename);
-    // make telemetry decoder object
-    telemetry_decoder_ = beidou_b3i_make_telemetry_decoder_gs(satellite_, dump_);
-    DLOG(INFO) << "telemetry_decoder(" << telemetry_decoder_->unique_id() << ")";
-    channel_ = 0;
-    if (in_streams_ > 1)
-        {
-            LOG(ERROR) << "This implementation only supports one input stream";
-        }
-    if (out_streams_ > 1)
-        {
-            LOG(ERROR) << "This implementation only supports one output stream";
-        }
+BeidouB3iTelemetryDecoder::BeidouB3iTelemetryDecoder(
+    ConfigurationInterface *configuration, std::string role,
+    unsigned int in_streams, unsigned int out_streams)
+    : role_(role), in_streams_(in_streams), out_streams_(out_streams) {
+  std::string default_dump_filename = "./navigation.dat";
+  DLOG(INFO) << "role " << role;
+  dump_ = configuration->property(role + ".dump", false);
+  dump_filename_ =
+      configuration->property(role + ".dump_filename", default_dump_filename);
+  // make telemetry decoder object
+  telemetry_decoder_ = beidou_b3i_make_telemetry_decoder_gs(satellite_, dump_);
+  DLOG(INFO) << "telemetry_decoder(" << telemetry_decoder_->unique_id() << ")";
+  channel_ = 0;
+  if (in_streams_ > 1) {
+    LOG(ERROR) << "This implementation only supports one input stream";
+  }
+  if (out_streams_ > 1) {
+    LOG(ERROR) << "This implementation only supports one output stream";
+  }
 }
 
+BeidouB3iTelemetryDecoder::~BeidouB3iTelemetryDecoder() = default;
 
-BeidouB3iTelemetryDecoder::~BeidouB3iTelemetryDecoder()
-{
+void BeidouB3iTelemetryDecoder::set_satellite(const Gnss_Satellite &satellite) {
+  satellite_ = Gnss_Satellite(satellite.get_system(), satellite.get_PRN());
+  telemetry_decoder_->set_satellite(satellite_);
+  DLOG(INFO) << "TELEMETRY DECODER: satellite set to " << satellite_;
 }
 
-
-void BeidouB3iTelemetryDecoder::set_satellite(const Gnss_Satellite& satellite)
-{
-    satellite_ = Gnss_Satellite(satellite.get_system(), satellite.get_PRN());
-    telemetry_decoder_->set_satellite(satellite_);
-    DLOG(INFO) << "TELEMETRY DECODER: satellite set to " << satellite_;
+void BeidouB3iTelemetryDecoder::connect(gr::top_block_sptr top_block) {
+  if (top_block) { /* top_block is not null */
+  };
+  // Nothing to connect internally
+  DLOG(INFO) << "nothing to connect internally";
 }
 
-
-void BeidouB3iTelemetryDecoder::connect(gr::top_block_sptr top_block)
-{
-    if (top_block)
-        { /* top_block is not null */
-        };
-    // Nothing to connect internally
-    DLOG(INFO) << "nothing to connect internally";
+void BeidouB3iTelemetryDecoder::disconnect(gr::top_block_sptr top_block) {
+  if (top_block) { /* top_block is not null */
+  };
+  // Nothing to disconnect
 }
 
-
-void BeidouB3iTelemetryDecoder::disconnect(gr::top_block_sptr top_block)
-{
-    if (top_block)
-        { /* top_block is not null */
-        };
-    // Nothing to disconnect
+gr::basic_block_sptr BeidouB3iTelemetryDecoder::get_left_block() {
+  return telemetry_decoder_;
 }
 
-
-gr::basic_block_sptr BeidouB3iTelemetryDecoder::get_left_block()
-{
-    return telemetry_decoder_;
-}
-
-
-gr::basic_block_sptr BeidouB3iTelemetryDecoder::get_right_block()
-{
-    return telemetry_decoder_;
+gr::basic_block_sptr BeidouB3iTelemetryDecoder::get_right_block() {
+  return telemetry_decoder_;
 }

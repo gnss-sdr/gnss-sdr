059fbfedc209dabdc531c2303d40567222de91bd
Marc Majoral <mmajoral@cttc.es>
2019-04-12 15:26:47 +0200

    changed default fft/ifft block exp

--- src/algorithms/acquisition/adapters/gps_l5i_pcps_acquisition_fpga.cc
+++ src/algorithms/acquisition/adapters/gps_l5i_pcps_acquisition_fpga.cc
@@ -161,7 +161,7 @@ GpsL5iPcpsAcquisitionFpga::GpsL5iPcpsAcquisitionFpga(
     acq_parameters.all_fft_codes = d_all_fft_codes_;
 
     // reference for the FPGA FFT-IFFT attenuation factor
-    acq_parameters.total_block_exp = configuration_->property(role + ".total_block_exp", 12);
+    acq_parameters.total_block_exp = configuration_->property(role + ".total_block_exp", 13);
 
     acq_parameters.num_doppler_bins_step2 = configuration_->property(role + ".second_nbins", 4);
     acq_parameters.doppler_step2 = configuration_->property(role + ".second_doppler_step", 125.0);
@@ -172,7 +172,7 @@ GpsL5iPcpsAcquisitionFpga::GpsL5iPcpsAcquisitionFpga(
     channel_ = 0;
     doppler_step_ = 0;
     gnss_synchro_ = nullptr;
-    
+
     // temporary buffers that we can delete
     delete[] code;
     delete fft_if;

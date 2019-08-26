881188d274c43d801e276a2919c861a5aea4ec0f
lucasventurar <lucas.ventura.r@gmail.com>
2019-08-26 12:47:37 +0200

    Enter is deleted

--- src/algorithms/telemetry_decoder/gnuradio_blocks/gps_l1_ca_telemetry_decoder_gs.cc
+++ src/algorithms/telemetry_decoder/gnuradio_blocks/gps_l1_ca_telemetry_decoder_gs.cc
@@ -384,7 +384,6 @@ int gps_l1_ca_telemetry_decoder_gs::general_work(int noutput_items __attribute__
                     detection = _Lambda(corr_value1, corr_value2, 0);
                 else  // Hard correlation
                     detection = _Lambda(corr_value, 0, d_samples_per_preamble);
-
                 if (detection)
                     {
                         d_preamble_index = d_sample_counter;  // record the preamble sample stamp

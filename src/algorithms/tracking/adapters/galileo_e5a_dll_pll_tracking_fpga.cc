781f83122bb55790a7b80aa5cd07b21aa1b645a8
Marc Majoral <majoralmarc@gmail.com>
2019-10-03 15:26:58 +0200

    replaced the device_base parameter by the dev_file_num parameter. dev_file_num specifies for each GNSS signal the first /dev/uio<X> driver that is associated to an FPGA HW accelerator that can be used for the tracking process of that GNSS signal. This parameter depends on the FPGA compilation and it is independant of the number of channels that are used for the tracking of other GNSS signals.

--- src/algorithms/tracking/adapters/galileo_e5a_dll_pll_tracking_fpga.cc
+++ src/algorithms/tracking/adapters/galileo_e5a_dll_pll_tracking_fpga.cc
@@ -176,8 +176,9 @@ GalileoE5aDllPllTrackingFpga::GalileoE5aDllPllTrackingFpga(
     std::string default_device_name = "/dev/uio";
     std::string device_name = configuration->property(role + ".devicename", default_device_name);
     trk_param_fpga.device_name = device_name;
-    // obtain the number of the first uio device file that is assigned to the FPGA E5a tracking multicorrelator HW accelerators
-    trk_param_fpga.dev_file_num = configuration->property(role + ".dev_file_num", 38);
+    // obtain the number of the first uio device corresponding to a HW accelerator in the FPGA
+    // that can be assigned to the tracking of the E5a signal
+    trk_param_fpga.dev_file_num = configuration->property(role + ".dev_file_num", 27);
     // compute the number of tracking channels that have already been instantiated. The order in which
     // GNSS-SDR instantiates the tracking channels i L1, L2, L5, E1, E5a
     trk_param_fpga.num_prev_assigned_ch = configuration->property("Channels_1C.count", 0) +

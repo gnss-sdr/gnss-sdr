43a8090dc66098bd5c53c881f6a6d4b781d9addc
Carles Fernandez <carles.fernandez@gmail.com>
2019-10-09 09:18:29 +0200

    Add work in the AD9361 manager

--- src/algorithms/signal_source/libs/ad9361_manager.h
+++ src/algorithms/signal_source/libs/ad9361_manager.h
@@ -37,6 +37,7 @@
 #include <cstdint>
 #include <string>
 
+#define FIR_BUF_SIZE 8192
 
 /* RX is input, TX is output */
 enum iodev
@@ -48,7 +49,7 @@ enum iodev
 /* common RX and TX streaming params */
 struct stream_cfg
 {
-    int64_t bw_hz;       // Analog banwidth in Hz
+    int64_t bw_hz;       // Analog bandwidth in Hz
     int64_t fs_hz;       // Baseband sample rate in Hz
     int64_t lo_hz;       // Local oscillator frequency in Hz
     const char *rfport;  // Port name
@@ -63,9 +64,6 @@ void wr_ch_lli(struct iio_channel *chn, const char *what, int64_t val);
 
 /* write attribute: string */
 void wr_ch_str(struct iio_channel *chn, const char *what, const char *str);
-
-/* helper function generating channel names */
-char *get_ch_name(const char *type, int id, char *tmpstr);
 
 /* returns ad9361 phy device */
 struct iio_device *get_ad9361_phy(struct iio_context *ctx);
@@ -127,11 +125,58 @@ bool config_ad9361_lo_remote(const std::string &remote_host,
     double scale_dds_dbfs_,
     double phase_dds_deg_);
 
-
 bool ad9361_disable_lo_remote(const std::string &remote_host);
 
 bool ad9361_disable_lo_local();
 
 bool load_fir_filter(std::string &filter, struct iio_device *phy);
+
+int ad9361_set_bb_rate_custom_filter_manual(struct iio_device *dev,
+    uint64_t rate, uint64_t Fpass,
+    uint64_t Fstop, uint64_t wnom_tx, uint64_t wnom_rx);
+
+int ad9361_set_bb_rate_custom_filter_auto(struct iio_device *dev,
+    uint64_t rate);
+
+int apply_custom_filter(struct iio_device *dev, unsigned dec_tx,
+    unsigned dec_rx, short *tapsTx,
+    short *tapsRx, unsigned taps,
+    uint64_t rate,
+    int gain_tx, int gain_rx,
+    uint64_t wnom_tx, uint64_t wnom_rx);
+
+int ad9361_calculate_rf_clock_chain(uint64_t sample_rate,
+    uint64_t rate_gov,
+    uint64_t *rx_path_clks,
+    uint64_t *tx_path_clks);
+
+int determine_path_rates_with_fir(uint64_t sample_rate,
+    uint64_t rate_gov,
+    uint64_t *rx_path_clks,
+    uint64_t *tx_path_clks,
+    uint64_t tmp,
+    int FIR);
+
+bool check_rates(int FIR, const int *HB_configs, uint64_t samp_rate,
+    uint64_t *rates);
+
+int determine_pll_div(uint64_t *rates);
+
+int check_dac_adc_config(uint64_t pll_bb, int PLL_mult,
+    int dec_table_index);
+
+double calculate_rfbw(double pll_rate, double caldiv, bool TX,
+    double *rcaldiv);
+
+int build_configuration(struct filter_design_parameters *fdpTX,
+    struct filter_design_parameters *fdpRX,
+    uint64_t sample_rate,
+    uint64_t Fpass,
+    uint64_t Fstop,
+    uint64_t wnomTX,
+    uint64_t wnomRX);
+
+void set_max_taps(struct filter_design_parameters *fdpTX,
+    struct filter_design_parameters *fdpRX);
 
 #endif  // GNSS_SDR_AD9361_MANAGER_H_

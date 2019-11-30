0038853c521ed12f8a041462e4392b401a570369
Carles Fernandez <carles.fernandez@gmail.com>
2019-11-30 20:30:22 +0100

    Clean CMake files

--- cmake/Modules/SetupPython.cmake
+++ cmake/Modules/SetupPython.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2018 (see AUTHORS file for a list of contributors)
+# Copyright (C) 2011-2019 (see AUTHORS file for a list of contributors)
 #
 # This file is part of GNSS-SDR.
 #

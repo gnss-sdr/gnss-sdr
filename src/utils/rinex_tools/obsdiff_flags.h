b66d8066c3c99737ea5da1257b27f26acbc08b94
Javier <javiarribas@gmail.com>
2020-02-18 16:45:56 +0100

    Adding a new RINEX observables testing tool to command line utilities set

---
+++ src/utils/rinex_tools/obsdiff_flags.h
@@ -0,0 +1,46 @@
+/*!
+ * \file rinex_observable_tests_flags.h
+ * \brief Helper file for unit testing
+ * \author Javier Arribas, 2019. jarribas(at)cttc.es
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2019  (see AUTHORS file for a list of contributors)
+ *
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GNSS-SDR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNSS-SDR. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+
+#ifndef GNSS_SDR_OBS_DIFF_FLAGS_H_
+#define GNSS_SDR_OBS_DIFF_FLAGS_H_
+
+#include <gflags/gflags.h>
+#include <limits>
+
+DEFINE_double(skip_obs_transitory_s, 30.0, "Skip the initial observable outputs to avoid transitory results [s]");
+DEFINE_double(skip_obs_ends_s, 5.0, "Skip the lasts observable outputs to avoid transitory results [s]");
+DEFINE_bool(single_diffs, false, "Compute also the single difference errors for Accumulated Carrier Phase and Carrier Doppler (requires LO synchronization between receivers)");
+DEFINE_bool(compare_with_5X, false, "Compare the E5a Doppler and Carrier Phases with the E5 full bw in RINEX (expect discrepancy due to the center frequencies differences");
+DEFINE_bool(dupli_sat, false, "Enable special observable test mode where the scenario contains duplicated satellite orbits");
+DEFINE_string(dupli_sat_prns, "1,2,3,4", "List of duplicated satellites PRN pairs (i.e. 1,2,3,4 indicates that the PRNs 1,2 share the same orbit. The same applies for PRNs 3,4)");
+DEFINE_string(ref_rinex_obs, "reference.obs", "Filename of reference RINEX navigation file");
+DEFINE_string(test_rinex_obs, "test.obs", "Filename of test RINEX navigation file");
+
+#endif

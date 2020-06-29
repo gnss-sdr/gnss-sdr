3bc8595dfe6ee00e756f77b9f903153e35b99e3b
Carles Fernandez <carles.fernandez@gmail.com>
2020-06-29 10:13:07 +0200

    Add inner_product to detector benchmark

--- src/tests/benchmarks/benchmark_detector.cc
+++ src/tests/benchmarks/benchmark_detector.cc
@@ -20,18 +20,19 @@
 
 #include "GPS_L1_CA.h"
 #include <benchmark/benchmark.h>
-#include <algorithm>
+#include <algorithm>  // for std::generate
 #include <array>
+#include <cmath>  // std::for signbit
 #include <cstdint>
-#include <numeric>
+#include <functional>  // for std::plus
+#include <numeric>     // for std::accumulate, std::inner_product
 #include <random>
 #include <vector>
-
 
 void bm_forloop(benchmark::State& state)
 {
     std::vector<float> d_symbol_history(GPS_CA_PREAMBLE_LENGTH_SYMBOLS, 0.0);
-    std::array<int32_t, GPS_CA_PREAMBLE_LENGTH_BITS> d_preamble_samples{};
+    std::array<int32_t, GPS_CA_PREAMBLE_LENGTH_SYMBOLS> d_preamble_samples{};
 
     // fill the inputs
     std::random_device rd;
@@ -39,12 +40,12 @@ void bm_forloop(benchmark::State& state)
     std::uniform_real_distribution<> dist(-1.0, 1.0);
     std::generate(d_symbol_history.begin(), d_symbol_history.end(), [&dist, &e2]() { return dist(e2); });
 
-    std::generate(d_preamble_samples.begin(), d_preamble_samples.end(), [n = 0]() mutable { return (GPS_CA_PREAMBLE[n++] == '1' ? 1 : -1); });
+    std::generate(d_preamble_samples.begin(), d_preamble_samples.end(), [n = 0]() mutable { return (GPS_CA_PREAMBLE_SYMBOLS_STR[n++] == '1' ? 1 : -1); });
 
     while (state.KeepRunning())
         {
             int32_t corr_value = 0;
-            for (int32_t i = 0; i < GPS_CA_PREAMBLE_LENGTH_BITS; i++)
+            for (size_t i = 0; i < d_preamble_samples.size(); i++)
                 {
                     if (d_symbol_history[i] < 0.0)
                         {
@@ -62,7 +63,7 @@ void bm_forloop(benchmark::State& state)
 void bm_accumulate(benchmark::State& state)
 {
     std::vector<float> d_symbol_history(GPS_CA_PREAMBLE_LENGTH_SYMBOLS, 0.0);
-    std::array<int32_t, GPS_CA_PREAMBLE_LENGTH_BITS> d_preamble_samples{};
+    std::array<int32_t, GPS_CA_PREAMBLE_LENGTH_SYMBOLS> d_preamble_samples{};
 
     // fill the inputs
     std::random_device rd;
@@ -70,19 +71,46 @@ void bm_accumulate(benchmark::State& state)
     std::uniform_real_distribution<> dist(-1.0, 1.0);
     std::generate(d_symbol_history.begin(), d_symbol_history.end(), [&dist, &e2]() { return dist(e2); });
 
-    std::generate(d_preamble_samples.begin(), d_preamble_samples.end(), [n = 0]() mutable { return (GPS_CA_PREAMBLE[n++] == '1' ? 1 : -1); });
+    std::generate(d_preamble_samples.begin(), d_preamble_samples.end(), [n = 0]() mutable { return (GPS_CA_PREAMBLE_SYMBOLS_STR[n++] == '1' ? 1 : -1); });
 
     while (state.KeepRunning())
         {
             int32_t corr_value = 0;
             corr_value += std::accumulate(d_symbol_history.begin(),
-                d_symbol_history.begin() + GPS_CA_PREAMBLE_LENGTH_BITS,
+                d_symbol_history.end(),
                 0,
                 [&d_preamble_samples, n = 0](float a, float b) mutable { return (b > 0.0 ? a + d_preamble_samples[n++] : a - d_preamble_samples[n++]); });
         }
 }
 
 
+void bm_inner_product(benchmark::State& state)
+{
+    std::vector<float> d_symbol_history(GPS_CA_PREAMBLE_LENGTH_SYMBOLS, 0.0);
+    std::array<int32_t, GPS_CA_PREAMBLE_LENGTH_SYMBOLS> d_preamble_samples{};
+
+    // fill the inputs
+    std::random_device rd;
+    std::default_random_engine e2(rd());
+    std::uniform_real_distribution<> dist(-1.0, 1.0);
+    std::generate(d_symbol_history.begin(), d_symbol_history.end(), [&dist, &e2]() { return dist(e2); });
+
+    std::generate(d_preamble_samples.begin(), d_preamble_samples.end(), [n = 0]() mutable { return (GPS_CA_PREAMBLE_SYMBOLS_STR[n++] == '1' ? 1 : -1); });
+
+    while (state.KeepRunning())
+        {
+            int32_t corr_value = 0;
+            corr_value += std::inner_product(d_symbol_history.begin(),
+                d_symbol_history.end(),
+                d_preamble_samples.begin(),
+                0,
+                std::plus<>(),
+                [&](float a, float b) { return (std::signbit(a) ? -b : b); });
+        }
+}
+
+
 BENCHMARK(bm_forloop);
 BENCHMARK(bm_accumulate);
+BENCHMARK(bm_inner_product);
 BENCHMARK_MAIN();

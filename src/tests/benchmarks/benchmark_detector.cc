c46cd87d377a52b2ccb0cccb0266e83af292f06e
Carles Fernandez <carles.fernandez@gmail.com>
2020-07-01 23:05:33 +0200

    Experiment with transform_reduce

--- src/tests/benchmarks/benchmark_detector.cc
+++ src/tests/benchmarks/benchmark_detector.cc
@@ -28,6 +28,9 @@
 #include <numeric>     // for std::accumulate, std::inner_product
 #include <random>
 #include <vector>
+#if COMPILER_HAS_STD_TRANSFORM_REDUCE_WITH_POLICY
+#include <execution>
+#endif
 
 void bm_forloop(benchmark::State& state)
 {
@@ -113,7 +116,72 @@ void bm_inner_product(benchmark::State& state)
         }
 }
 
+
+#if COMPILER_HAS_STD_TRANSFORM_REDUCE
+void bm_transform_reduce(benchmark::State& state)
+{
+    std::vector<float> d_symbol_history(GPS_CA_PREAMBLE_LENGTH_SYMBOLS, 0.0);
+    std::array<int32_t, GPS_CA_PREAMBLE_LENGTH_SYMBOLS> d_preamble_samples{};
+
+    // fill the inputs
+    std::random_device rd;
+    std::default_random_engine e2(rd());
+    std::uniform_real_distribution<> dist(-1.0, 1.0);
+    std::generate(d_symbol_history.begin(), d_symbol_history.end(), [&dist, &e2]() { return dist(e2); });
+
+    std::generate(d_preamble_samples.begin(), d_preamble_samples.end(), [n = 0]() mutable { return (GPS_CA_PREAMBLE_SYMBOLS_STR[n++] == '1' ? 1 : -1); });
+
+    while (state.KeepRunning())
+        {
+            int32_t corr_value = 0;
+            corr_value += std::transform_reduce(d_symbol_history.begin(),
+                d_symbol_history.end(),
+                d_preamble_samples.begin(),
+                0,
+                std::plus<>(),
+                [](auto a, auto b) { return (std::signbit(a) ? -b : b); });
+        }
+}
+#endif
+
+
+#if COMPILER_HAS_STD_TRANSFORM_REDUCE_WITH_POLICY
+void bm_transform_reduce_policy(benchmark::State& state)
+{
+    std::vector<float> d_symbol_history(GPS_CA_PREAMBLE_LENGTH_SYMBOLS, 0.0);
+    std::array<int32_t, GPS_CA_PREAMBLE_LENGTH_SYMBOLS> d_preamble_samples{};
+
+    // fill the inputs
+    std::random_device rd;
+    std::default_random_engine e2(rd());
+    std::uniform_real_distribution<> dist(-1.0, 1.0);
+    std::generate(d_symbol_history.begin(), d_symbol_history.end(), [&dist, &e2]() { return dist(e2); });
+
+    std::generate(d_preamble_samples.begin(), d_preamble_samples.end(), [n = 0]() mutable { return (GPS_CA_PREAMBLE_SYMBOLS_STR[n++] == '1' ? 1 : -1); });
+
+    while (state.KeepRunning())
+        {
+            int32_t corr_value = 0;
+            corr_value += std::transform_reduce(
+                std::execution::par,
+                d_symbol_history.begin(),
+                d_symbol_history.end(),
+                d_preamble_samples.begin(),
+                0,
+                std::plus<>(),
+                [](auto a, auto b) { return (std::signbit(a) ? -b : b); });
+        }
+}
+#endif
+
+
 BENCHMARK(bm_forloop);
 BENCHMARK(bm_accumulate);
 BENCHMARK(bm_inner_product);
+#if COMPILER_HAS_STD_TRANSFORM_REDUCE
+BENCHMARK(bm_transform_reduce);
+#endif
+#if COMPILER_HAS_STD_TRANSFORM_REDUCE_WITH_POLICY
+BENCHMARK(bm_transform_reduce_policy);
+#endif
 BENCHMARK_MAIN();

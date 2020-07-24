fa9abed435574b682ae428485b11122aef29dc0a
piyush0411 <piyush04111999@gmail.com>
2020-07-25 02:56:14 +0530

    Added missing parameter

--- src/core/system_parameters/Galileo_E5b.h
+++ src/core/system_parameters/Galileo_E5b.h
@@ -33,6 +33,7 @@ constexpr double GALILEO_E5B_CODE_CHIP_RATE_CPS = 1.023e7;    //!< Galileo E5b c
 constexpr double GALILEO_E5B_I_TIERED_CODE_PERIOD_S = 0.004;  //!< Galileo E5b-I tiered code period [s]
 constexpr double GALILEO_E5B_Q_TIERED_CODE_PERIOD_S = 0.100;  //!< Galileo E5b-Q tiered code period [s]
 constexpr double GALILEO_E5B_CODE_PERIOD_S = 0.001;           //!< Galileo E5b primary code period [s]
+constexpr int32_t GALILEO_E5B_CODE_PERIOD_MS = 1;             //!< Galileo E5b primary code period [ms]
 constexpr int32_t GALILEO_E5B_CODE_LENGTH_CHIPS = 10230;      //!< Galileo E5b primary code length [chips]
 constexpr int32_t GALILEO_E5B_I_SECONDARY_CODE_LENGTH = 4;    //!< Galileo E5b-I secondary code length [chips]
 constexpr int32_t GALILEO_E5B_Q_SECONDARY_CODE_LENGTH = 100;  //!< Galileo E5b-Q secondary code length [chips]

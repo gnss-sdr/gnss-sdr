3c17f0a9a7897b983b90f501a34c18da731e05e8
Carles Fernandez <carles.fernandez@gmail.com>
2021-01-29 14:01:21 +0100

    Fix defect detected by Coverity Scan (uninitialized scalar variables)

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/apps/volk_gnsssdr_profile.cc
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/apps/volk_gnsssdr_profile.cc
@@ -232,7 +232,7 @@ void read_results(std::vector<volk_gnsssdr_test_results_t> *results, std::string
 
                     if (single_kernel_result.size() == 3)
                         {
-                            volk_gnsssdr_test_results_t kernel_result;
+                            volk_gnsssdr_test_results_t kernel_result{};
                             kernel_result.name = std::string(single_kernel_result[0]);
                             kernel_result.config_name = std::string(single_kernel_result[0]);
                             kernel_result.best_arch_u = std::string(single_kernel_result[1]);

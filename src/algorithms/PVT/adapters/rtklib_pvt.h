11f53a3fcb4ad64ea71c0daf38fd3c79d6ac7832
Jim Melton <jwmelto@users.sourceforge.net>
2021-09-22 18:12:30 -0600

    document PVT params; enable RTKLIB trace level to be set

--- src/algorithms/PVT/adapters/rtklib_pvt.h
+++ src/algorithms/PVT/adapters/rtklib_pvt.h
@@ -44,6 +44,131 @@ class Gps_Ephemeris;
 
 /*!
  * \brief This class implements a PvtInterface for the RTKLIB PVT block
+ *
+ * Global configuration options used:
+ *
+ * GNSS-SDR.pre_2009_file - flag indicating a file older than 2009 rollover should be processed (false)
+ * GNSS-SDR.observable_interval_ms - (20)
+ *
+ * It supports the following configuration options:
+ *
+ *  .dump - (false)
+ *  .dump_filename - ("./pvt.dat")
+ *  .dump_mat - (true)
+ *  .rtk_trace_level - debug level for the RTKLIB methods (0)
+ *
+ *  .output_rate_ms - (500)
+ *                    Note that the actual rate is the least common multiple of this value and GNSS-SDR.observable_interval_ms
+ *  .display_rate_ms - (500)
+ *
+ *  .flag_nmea_tty_port - (false)
+ *  .nmea_dump_filename - ("./nmea_pvt.nmea")
+ *  .nmea_dump_devname - ("/dev/tty1")
+ *
+ *  .rinex_version - (3) overridden by -RINEX_version=n.nn command line argument
+ *  .rinexobs_rate_ms - rate at which RINEX observations are written (1000).  Note that
+ *                      the actual rate is the least common multiple of this value and
+ *                      .output_rate_ms
+ *  .rinex_name - (-RINEX_name command-line argument)
+ *
+ *  .flag_rtcm_tty_port - (false)
+ *  .rtcm_dump_devname - ("/dev/pts/1")
+ *  .flag_rtcm_server - (false)
+ *  .rtcm_tcp_port - (2101)
+ *  .rtcm_station_id - (1234)
+ * Output rates ... all values are LCM with the computed output rate (above)
+ *  .rtcm_MT1019_rate_ms - (5000)
+ *  .rtcm_MT1020_rate_ms - (5000)
+ *  .rtcm_MT1045_rate_ms - (5000)
+ *  .rtcm_MSM_rate_ms - (1000)
+ *  .rtcm_MT1077_rate_ms - (.rtcm_MSM_rate_ms)
+ *  .rtcm_MT1087_rate_ms - (.rtcm_MSM_rate_ms)
+ *  .rtcm_MT1097_rate_ms - (.rtcm_MSM_rate_ms)
+ *
+ *  .kml_rate_ms - (1000)
+ *  .gpx_rate_ms - (1000)
+ *  .geojson_rate_ms - (1000)
+ *  .nmea_rate_ms - (1000)
+ *
+ *  .positioning_mode - The RTKLIB positioning mode. ("Single") Supported values are "Single",
+ *                      "Static", "Kinematic", "PPP_Static" and "PPP_Kinematic". Unsupported modes
+ *                      include DGPS/DGNSS, Moving Baseline, Fixed, and PPP-fixed
+ *  .num_bands - number of frequencies to use, between 1 and 3. Default is based on the channels configured
+ *  .elevation_mask - (15.0). Value must be in the range [0,90.0]
+ *  .dynamics_model - (0) 0:none, 1:velocity, 2:acceleration
+
+ *  .iono_model - ("OFF"). Supported values are "OFF", "Broadcast", "SBAS", "Iono-Free-LC",
+ *                      "Estimate_STEC", "IONEX". Unsupported values include QZSS broadcast, QZSS
+ *                      LEX, and SLANT TEC.
+ *  .trop_model - ("OFF"). Supported values are "OFF", "Saastamoinen", "SBAS", "Estimate_ZTD", and
+ *                      "Estimate_ZTD_Grad". Unsupported values include ZTD correction and ZTD+grad
+ *                      correction
+ *  .phwindup - phase windup correction for PPP modes (0)
+ *  .reject_GPS_IIA - whether the GPS Block IIA satellites in eclipse are excluded (0). Only applies in PPP-* modes
+ *  .raim_fde - whether RAIM (receiver autonomous integrity monitoring) FDE (fault detection and exclusion) is enabled (0)
+ *  .earth_tide - (0)
+ *  .navigation_system - mask of navigation systems to use. Default based on configured channels
+ *                      0x01:GPS, 0x02:SBAS, 0x04:GLONASS, 0x08:Galileo, 0x10:QZSS, 0x20:BeiDou,
+ *                      0x40:IRNS, 0x80:LEO
+ *
+ *  .AR_GPS - Ambiguity Resolution mode for GPS ("Continuous"). Supported values are "OFF",
+ *                      "Continuous", "Instantaneous", "Fix-and-Hold", "PPP-AR". Unsupported values
+ *                      include PPP-AR ILS, WLNL, and TCAR.
+ *  .AR_GLO - Ambiguity Resolution mode for GLONASS (1). Value must be in the range [0,3]. (0:off,1:on,2:auto cal,3:ext cal)
+ *  .AR_DBS - Ambiguity Resolution Mode for BeiDou (1). Value must be in the range [0,1]. (0:off,1:on)
+ *  .min_ratio_to_fix_ambiguity - (3.0)
+ *  .min_lock_to_fix_ambiguity - (0)
+ *  .min_elevation_to_fix_ambiguity - minimum elevation (deg) to fix integer ambiguity (0.0)
+ *  .outage_reset_ambiguity - (5)
+ *  .slip_threshold - (0.05)
+ *  .threshold_reject_gdop - if GDOP is over this value, the observable is excluded (30.0)
+ *  .threshold_reject_innovation - if innovation is over this value, the observable is excluded (30.0)
+ *  .number_filter_iter - number of iterations for the estimation filter (1)
+ *  .bias_0 - (30.0)
+ *  .iono_0 - (0.03)
+ *  .trop_0 - (0.3)
+ *  .sigma_bias - process noise stddev of carrier-phase bias(ambiguity)(cycle/sqrt(s)) (1e-4)
+ *  .sigma_iono - process noise stddev of vertical ionospheric delay per 10km baseline (m/sqrt(s)) (1e-3)
+ *  .sigma_trop - process noise stddev of zenith tropospheric delay (m/sqrt(s)) (1e-4)
+ *  .sigma_acch - process noise stddev of the receiver acceleration horizontal component (m/s2/sqrt(s)) (1e-1)
+ *  .sigma_accv - process noise stddev of the receiver acceleration vertical component (m/s2/sqrt(s)) (1e-2)
+ *  .sigma_pos - (0.0)
+ *  .code_phase_error_ratio_l1 - (100.0)
+ *  .code_phase_error_ratio_l2 - (100.0)
+ *  .code_phase_error_ratio_l5 - (100.0)
+ *  .carrier_phase_error_factor_a - (0.003)
+ *  .carrier_phase_error_factor_b - (0.003)
+ *
+ *  .output_enabled - (true)
+ *  .rinex_output_enabled - (.output_enabled)
+ *  .gpx_output_enabled - (.output_enabled)
+ *  .geojson_output_enabled - (.output_enabled)
+ *  .kml_output_enabled - (.output_enabled)
+ *  .xml_output_enabled - (.output_enabled)
+ *  .nmea_output_enabled - (.output_enabled)
+ *  .rtcm_output_enabled - (false)
+
+ *  .output_path - directory to which output files are written (".")
+ *  .rinex_output_path - (.output_path)
+ *  .gpx_output_path - (.output_path)
+ *  .geojson_output_path - (.output_path)
+ *  .kml_output_path - (.output_path)
+ *  .xml_output_path - (.output_path)
+ *  .nmea_output_path - (.output_path)
+ *  .rtcm_output_path - (.output_path)
+ *
+ *  .enable_monitor - enable the PVT monitor (false)
+ *  .monitor_client_addresses - ("127.0.0.1")
+ *  .monitor_udp_port - DO NOT USE THE DEFAULT (1234)
+ *  .enable_protobuf - serialize using protocol buffers (true). Monitor.enable_protobuf if true, sets this to true
+ *
+ *  .enable_monitor_ephemeris - enable the ephemeris monitor (false)
+ *  .monitor_ephemeris_client_addresses - ("127.0.0.1")
+ *  .monitor_ephemeris_udp_port - DO NOT USE THE DEFAULT (1234)
+ *
+ *  .show_local_time_zone - (false)
+ *  .enable_rx_clock_correction - (false)
+ *  .max_clock_offset_ms - (40)
  */
 class Rtklib_Pvt : public PvtInterface
 {

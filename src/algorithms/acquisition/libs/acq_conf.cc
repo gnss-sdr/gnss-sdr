64da4a999641a61b636dfe6bf6d51626a7f37efe
iucgnaw <iucgnaw@msn.com>
2021-10-06 13:08:41 +0800

    Fixed default value of coherent_integration_time_ms property
    Signed-off-by: iucgnaw <iucgnaw@msn.com>

--- src/algorithms/acquisition/libs/acq_conf.cc
+++ src/algorithms/acquisition/libs/acq_conf.cc
@@ -70,7 +70,7 @@ void Acq_Conf::SetFromConfiguration(const ConfigurationInterface *configuration,
     const int64_t fs_in_deprecated = configuration->property("GNSS-SDR.internal_fs_hz", fs_in);
     fs_in = configuration->property("GNSS-SDR.internal_fs_sps", fs_in_deprecated);
     doppler_max = configuration->property(role + ".doppler_max", doppler_max);
-    sampled_ms = configuration->property(role + ".coherent_integration_time_ms", sampled_ms);
+    sampled_ms = configuration->property(role + ".coherent_integration_time_ms", ms_per_code);
     bit_transition_flag = configuration->property(role + ".bit_transition_flag", bit_transition_flag);
     max_dwells = configuration->property(role + ".max_dwells", max_dwells);
     dump = configuration->property(role + ".dump", dump);

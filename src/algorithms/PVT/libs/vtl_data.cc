edad601ed3ee4a1ddc20b389357af9f99988a8ab
miguekf <angel.glopez@alumnos.upm.es>
2023-03-11 21:43:57 +0100

    FIX: vtl_data debug_print() method

--- src/algorithms/PVT/libs/vtl_data.cc
+++ src/algorithms/PVT/libs/vtl_data.cc
@@ -54,15 +54,15 @@ void Vtl_Data::init_storage(int n_sats)
 void Vtl_Data::debug_print()
 {
     std::cout << "vtl_data debug print at RX TOW: " << epoch_tow_s << ", TRK sample counter: " << sample_counter << "\n";
-    // sat_p.print("VTL Sat Positions");
-    //sat_v.print("VTL Sat Velocities");
-    // sat_dts.print("VTL Sat clocks");
-    // sat_var.print("VTL Sat clock variances");
-    // sat_health_flag.print("VTL Sat health");
-    //sat_LOS.print("VTL SAT LOS");
+    sat_p.print("VTL Sat Positions");
+    sat_v.print("VTL Sat Velocities");
+    sat_dts.print("VTL Sat clocks");
+    sat_var.print("VTL Sat clock variances");
+    sat_health_flag.print("VTL Sat health");
+    sat_LOS.print("VTL SAT LOS");
     kf_state.print("EKF STATE");
 
-    //pr_m.print("Satellite Code pseudoranges [m]");
-    //doppler_hz.print("satellite Carrier Dopplers [Hz]");
-    // carrier_phase_rads.print("satellite accumulated carrier phases [rads]");
+    pr_m.print("Satellite Code pseudoranges [m]");
+    doppler_hz.print("satellite Carrier Dopplers [Hz]");
+    carrier_phase_rads.print("satellite accumulated carrier phases [rad]");
 }

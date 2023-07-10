fe13fc8f49f4bf5962cbdd1503a23cf8926b56f3
M.A. Gomez <gomezlma@inta.es>
2023-07-10 15:19:19 +0200

    [Fix] error

--- src/algorithms/PVT/libs/rtklib_solver.cc
+++ src/algorithms/PVT/libs/rtklib_solver.cc
@@ -899,7 +899,6 @@ void Rtklib_Solver::get_current_has_obs_correction(const std::string &signal, ui
 
 
 bool Rtklib_Solver::get_PVT(const std::map<int, Gnss_Synchro> &gnss_observables_map, double kf_update_interval_s)
-bool Rtklib_Solver::get_PVT(const std::map<int, Gnss_Synchro> &gnss_observables_map, double kf_update_interval_s)
 {
     std::map<int, Gnss_Synchro>::const_iterator gnss_observables_iter;
     std::map<int, Galileo_Ephemeris>::const_iterator galileo_ephemeris_iter;

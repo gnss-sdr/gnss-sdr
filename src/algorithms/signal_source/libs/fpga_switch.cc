f446bf3238b194bac8ad9a4aef0ef00b7000059a
Carles Fernandez <carles.fernandez@cttc.es>
2024-02-19 17:54:18 +0100

    Add missing include
    Fix compilation against glog-0.7.0

--- src/algorithms/signal_source/libs/fpga_switch.cc
+++ src/algorithms/signal_source/libs/fpga_switch.cc
@@ -25,6 +25,7 @@
 #include <fcntl.h>     // for open, O_RDWR, O_SYNC
 #include <iostream>    // for cout
 #include <sys/mman.h>  // for mmap
+#include <unistd.h>    // for close
 
 Fpga_Switch::Fpga_Switch(const std::string &device_name)
 {

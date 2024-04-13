fd5174e7ecce6f57073a0cd6cefa2e80958ac582
Carles Fernandez <carles.fernandez@cttc.es>
2024-04-13 09:03:46 +0200

    Remove duplicate libraries warning emitted by Xcode 15

--- cmake/Modules/XcodeRemoveWarningDuplicates.cmake
+++ cmake/Modules/XcodeRemoveWarningDuplicates.cmake
@@ -4,12 +4,12 @@
 # SPDX-FileCopyrightText: 2011-2024 C. Fernandez-Prades cfernandez(at)cttc.es
 # SPDX-License-Identifier: BSD-3-Clause
 
-if(DEFINED __INCLUDED_XCODE_DELETE_DUPLICATES_CMAKE)
+if(DEFINED __INCLUDED_XCODE_REMOVE_WARNING_DUPLICATES_CMAKE)
     return()
 endif()
-set(__INCLUDED_XCODE_DELETE_DUPLICATES_CMAKE TRUE)
+set(__INCLUDED_XCODE_REMOVE_WARNING_DUPLICATES_CMAKE TRUE)
 
-function(xcode_delete_duplicates target)
+function(xcode_remove_warning_duplicates target)
     if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
         if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "15.0.0")
             # A bug in Xcode 15 adds duplicate flags to the linker. In addition, the

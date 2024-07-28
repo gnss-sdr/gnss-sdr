42c2e3c2deb3d037e2af4dbf56f987ad2b966816
Victor Castillo <victorcastilloaguero@gmail.com>
2024-07-29 00:26:50 +0200

    Added extra data source
    which can be connected after a Signal Source in order to attach extra data (such as IMU measurements) to the sample stream.

---
+++ src/algorithms/signal_source/gnuradio_blocks/extra_data_source.cc
@@ -0,0 +1,82 @@
+/*!
+ * \file extra_data_source.cc
+ * \brief  GNURadio block that adds extra data to the sample stream.
+ * \author Victor Castillo, 2024. victorcastilloaguero(at).gmail.es
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * GNSS-SDR is a Global Navigation Satellite System software-defined receiver.
+ * This file is part of GNSS-SDR.
+ *
+ * Copyright (C) 2010-2021  (see AUTHORS file for a list of contributors)
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+#include "extra_data_source.h"
+#include <pmt/pmt.h>
+
+
+ExtraDataSource::ExtraDataSource(
+    const std::string& path,
+    const std::size_t& offset_in_file,
+    const std::size_t& item_size,
+    const bool& repeat,
+    const std::size_t& offset_in_samples,
+    const std::size_t& sample_period,
+    const gr::io_signature::sptr& io_signature
+    )
+    : gr::sync_block("Extra Data Source",
+          io_signature, io_signature),
+      extra_data_file_(
+          path,
+          offset_in_file,
+          item_size,
+          repeat),
+      offset_in_samples_(offset_in_samples),
+      sample_period_(sample_period),
+      last_tagged_sample_(offset_in_samples_)
+{
+    if (io_signature->min_streams() != 1 and io_signature->max_streams() != 1)
+        {
+            std::cout << "ERROR: This block only supports adding data to a single stream." << "\n";
+        }
+}
+
+std::vector<uint8_t> ExtraDataSource::get_next_item()
+{
+    return extra_data_file_.read_item();
+}
+
+std::size_t ExtraDataSource::get_offset_in_samples() const
+{
+    return offset_in_samples_;
+}
+
+std::size_t ExtraDataSource::get_sample_period() const
+{
+    return sample_period_;
+}
+
+int ExtraDataSource::work(int noutput_items,
+    gr_vector_const_void_star& input_items,
+    gr_vector_void_star& output_items) override
+{
+    const std::size_t ch = 0;
+    const int item_size = input_signature()->sizeof_stream_item(ch);
+    std::memcpy(output_items[ch], input_items[ch], noutput_items * item_size);
+
+    const uint64_t total_items_written = nitems_written(ch);
+    if (total_items_written >= sample_period_ + last_tagged_sample_)
+        {
+            for (uint64_t sample = last_tagged_sample_ + sample_period_; sample < total_items_written; sample += sample_period_)
+                {
+                    auto extra_data_item = get_next_item();
+                    add_item_tag(ch, sample, pmt::mp("extra_data"), pmt::init_u8vector(extra_data_item.size(), extra_data_item));
+                    last_tagged_sample_ = sample;
+                }
+        }
+
+    return noutput_items;
+}
---
+++ src/algorithms/signal_source/libs/extra_data_file.cc
@@ -0,0 +1,93 @@
+/*!
+ * \file extra_data_file.cc
+ * \brief  Provides a simple abstraction for reading contiguous binary data from a file
+ * \author Victor Castillo, 2024. victorcastilloaguero(at).gmail.es
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * GNSS-SDR is a Global Navigation Satellite System software-defined receiver.
+ * This file is part of GNSS-SDR.
+ *
+ * Copyright (C) 2010-2021  (see AUTHORS file for a list of contributors)
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+#include "extra_data_file.h"
+#include <cstring>
+
+ExtraDataFile::ExtraDataFile(
+    const std::string& path,
+    const std::size_t& offset_in_file,
+    const std::size_t& item_size,
+    const bool& repeat)
+    : path_(path),
+      file_(path_),
+      offset_in_file_(offset_in_file),
+      item_size_(item_size),
+      repeat_(repeat),
+      done_(false),
+      io_buffer_size_(item_size * IO_BUFFER_CAPACITY),
+      offset_in_io_buffer_(io_buffer_size_)  // Set to end of buffer so that first look up will trigger a read.
+{
+    file_.seekg(offset_in_file_, std::ios_base::beg);
+
+    io_buffer_.resize(io_buffer_size_);
+}
+
+void ExtraDataFile::reset()
+{
+    file_.seekg(offset_in_file_, std::ios_base::beg);
+    offset_in_io_buffer_ = io_buffer_size_;
+    done_ = false;
+}
+
+std::vector<uint8_t> ExtraDataFile::read_item()
+{
+    if (offset_in_io_buffer_ >= io_buffer_size_)
+        {
+            if (done_)
+                {
+                    return {};
+                }
+            else
+                {
+                    read_into_io_buffer();
+                }
+        }
+
+    std::vector<uint8_t> item_buf{};
+    read_into_item_buffer(item_buf);
+
+    return item_buf;
+}
+
+void ExtraDataFile::read_into_io_buffer()
+{
+    file_.get(reinterpret_cast<char*>(io_buffer_.data()), io_buffer_size_);
+    const std::size_t bytes_read = file_.gcount();
+
+    if (bytes_read < io_buffer_size_)
+        {
+            if (repeat_)
+                {
+                    reset();
+                    file_.get(reinterpret_cast<char*>(&io_buffer_[bytes_read]), io_buffer_size_ - bytes_read);
+                }
+            else
+                {
+                    io_buffer_size_ = bytes_read;
+                    done_ = true;
+                }
+        }
+
+    offset_in_io_buffer_ = 0;
+}
+
+void ExtraDataFile::read_into_item_buffer(std::vector<uint8_t>& item_buf)
+{
+    item_buf.resize(item_size_);
+    std::memcpy(item_buf.data(), &io_buffer_[offset_in_io_buffer_], item_size_);
+    offset_in_io_buffer_ += item_size_;
+}

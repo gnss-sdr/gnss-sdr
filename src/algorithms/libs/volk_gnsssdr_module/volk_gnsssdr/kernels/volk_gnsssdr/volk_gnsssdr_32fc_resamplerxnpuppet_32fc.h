47e0843ed0b6c36eac718bdce6380c64098663b0
Marcus Alagar <mvala079@gmail.com>
2025-08-25 19:30:37 -0500

    Implement 32fc xn resampler, puppet
    Implement 32-bit floating-point complex number resampler using
    RVV intrinsics using the same trick that worked with 16-bit
    integer complex numbers. Namely, interpreting the two
    32-bit components of the complex numbers as a single
    64-bit number for the purposes of moving around.
    
    Signed-off-by: Marcus Alagar <mvala079@gmail.com>

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_32fc_resamplerxnpuppet_32fc.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_32fc_resamplerxnpuppet_32fc.h
@@ -320,4 +320,33 @@ static inline void volk_gnsssdr_32fc_resamplerxnpuppet_32fc_neon(lv_32fc_t* resu
 }
 #endif
 
+
+#ifdef LV_HAVE_RVV
+static inline void volk_gnsssdr_32fc_resamplerxnpuppet_32fc_rvv(lv_32fc_t* result, const lv_32fc_t* local_code, unsigned int num_points)
+{
+    int code_length_chips = 2046;
+    float code_phase_step_chips = ((float)(code_length_chips) + 0.1) / ((float)num_points);
+    int num_out_vectors = 3;
+    float rem_code_phase_chips = -0.234;
+    int n;
+    float shifts_chips[3] = {-0.1, 0.0, 0.1};
+
+    lv_32fc_t** result_aux = (lv_32fc_t**)volk_gnsssdr_malloc(sizeof(lv_32fc_t*) * num_out_vectors, volk_gnsssdr_get_alignment());
+    for (n = 0; n < num_out_vectors; n++)
+        {
+            result_aux[n] = (lv_32fc_t*)volk_gnsssdr_malloc(sizeof(lv_32fc_t) * num_points, volk_gnsssdr_get_alignment());
+        }
+
+    volk_gnsssdr_32fc_xn_resampler_32fc_xn_rvv(result_aux, local_code, rem_code_phase_chips, code_phase_step_chips, shifts_chips, code_length_chips, num_out_vectors, num_points);
+
+    memcpy((lv_32fc_t*)result, (lv_32fc_t*)result_aux[0], sizeof(lv_32fc_t) * num_points);
+
+    for (n = 0; n < num_out_vectors; n++)
+        {
+            volk_gnsssdr_free(result_aux[n]);
+        }
+    volk_gnsssdr_free(result_aux);
+}
+#endif
+
 #endif  // INCLUDED_volk_gnsssdr_32fc_resamplerpuppet_32fc_H

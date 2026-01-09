6f1d9abd5c0f8e43c08cf4afa9f418db55fff554
Carles Fernandez <carles.fernandez@cttc.es>
2026-01-09 09:47:04 +0100

    Improve logging

--- src/algorithms/tracking/gnuradio_blocks/dll_pll_veml_tracking.cc
+++ src/algorithms/tracking/gnuradio_blocks/dll_pll_veml_tracking.cc
@@ -1850,7 +1850,8 @@ int dll_pll_veml_tracking::general_work(int noutput_items __attribute__((unused)
                 d_cn0_smoother.reset();
                 d_carrier_lock_test_smoother.reset();
 
-                LOG(INFO) << "Number of samples between Acquisition and Tracking = " << acq_trk_diff_samples << " ( " << acq_trk_diff_seconds << " s)";
+                LOG(INFO) << "Pull-in: Number of samples between Acquisition and Tracking = " << acq_trk_diff_samples << " ( " << acq_trk_diff_seconds << " s)"
+                          << "for satellite " << Gnss_Satellite(d_systemName, d_acquisition_gnss_synchro->PRN) << " in channel " << d_channel;
                 DLOG(INFO) << "PULL-IN Doppler [Hz] = " << d_carrier_doppler_hz
                            << ". PULL-IN Code Phase [samples] = " << d_acq_code_phase_samples;
 

4c07ea70eb84f56fadb340a2038df24c6506bda1
Carles Fernandez <carles.fernandez@cttc.es>
2026-05-24 23:34:19 +0200

    Fix formatting

--- utils/front-end-cal/main.cc
+++ utils/front-end-cal/main.cc
@@ -240,8 +240,7 @@ FrontEndCal_msg_rx::FrontEndCal_msg_rx()
     this->message_port_register_in(pmt::mp("events"));
 
 #if HAS_GENERIC_LAMBDA
-    this->set_msg_handler(pmt::mp("events"), [this](auto&& PH1)
-            { msg_handler_channel_events(std::forward<decltype(PH1)>(PH1)); });
+    this->set_msg_handler(pmt::mp("events"), [this](auto&& PH1) { msg_handler_channel_events(std::forward<decltype(PH1)>(PH1)); });
 #else
 #if USE_BOOST_BIND_PLACEHOLDERS
     this->set_msg_handler(

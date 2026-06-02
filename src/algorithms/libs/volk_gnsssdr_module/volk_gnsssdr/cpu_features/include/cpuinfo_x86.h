06a489ae4c42ab14ea8e12c8be797caf1ad0abed
Carles Fernandez <carles.fernandez@cttc.es>
2026-06-02 07:02:10 +0200

    Update local cpu_features to v0.11.0. Add support for AMD Zen 5, Intel Arrow Lake and Intel Lunar Lake microarchitectures

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/cpu_features/include/cpuinfo_x86.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/cpu_features/include/cpuinfo_x86.h
@@ -156,6 +156,8 @@ typedef enum
     INTEL_ADL,            // ALDER LAKE
     INTEL_RCL,            // ROCKET LAKE
     INTEL_RPL,            // RAPTOR LAKE
+    INTEL_LNL,            // LUNAR LAKE
+    INTEL_ARL,            // ARROW LAKE
     INTEL_KNIGHTS_M,      // KNIGHTS MILL
     INTEL_KNIGHTS_L,      // KNIGHTS LANDING
     INTEL_KNIGHTS_F,      // KNIGHTS FERRY
@@ -177,6 +179,7 @@ typedef enum
     AMD_ZEN2,             // K17 ZEN 2
     AMD_ZEN3,             // K19 ZEN 3
     AMD_ZEN4,             // K19 ZEN 4
+    AMD_ZEN5,             // K1A ZEN 5
     X86_MICROARCHITECTURE_LAST_,
 } X86Microarchitecture;
 

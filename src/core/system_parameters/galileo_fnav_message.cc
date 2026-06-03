a5edf388c1ea63988c9612f7d9241aba7c15e2a3
Carles Fernandez <carles.fernandez@cttc.es>
2026-06-03 12:51:36 +0200

    Fix the F/NAV BGD reading

--- src/core/system_parameters/galileo_fnav_message.cc
+++ src/core/system_parameters/galileo_fnav_message.cc
@@ -403,10 +403,11 @@ Galileo_Ephemeris Galileo_Fnav_Message::get_ephemeris() const
     ephemeris.toe = FNAV_t0e_3;            // Ephemeris reference time [s]
 
     // Clock correction parameters
-    ephemeris.toc = FNAV_t0c_1;  // Clock correction data reference Time of Week [sec]
-    ephemeris.af0 = FNAV_af0_1;  // SV clock bias correction coefficient [s]
-    ephemeris.af1 = FNAV_af1_1;  // SV clock drift correction coefficient [s/s]
-    ephemeris.af2 = FNAV_af2_1;  // SV clock drift rate correction coefficient [s/s^2]
+    ephemeris.toc = FNAV_t0c_1;        // Clock correction data reference Time of Week [sec]
+    ephemeris.af0 = FNAV_af0_1;        // SV clock bias correction coefficient [s]
+    ephemeris.af1 = FNAV_af1_1;        // SV clock drift correction coefficient [s/s]
+    ephemeris.af2 = FNAV_af2_1;        // SV clock drift rate correction coefficient [s/s^2]
+    ephemeris.BGD_E1E5a = FNAV_BGD_1;  // E1-E5a Broadcast Group Delay [s]
 
     // GST
     ephemeris.WN = FNAV_WN_3;    // Week number

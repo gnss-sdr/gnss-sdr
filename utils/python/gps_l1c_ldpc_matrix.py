3c35e4cab0c6f15b844a8078b1477e9d231c7868
tatjam <tatjam02@gmail.com>
2026-06-14 12:24:07 +0200

    LDPC data matrices
    Signed-off-by: tatjam <tatjam02@gmail.com>

---
+++ utils/python/gps_l1c_ldpc_matrix.py
@@ -0,0 +1,29 @@
+"""
+gps_l1c_ldpc_matrix.py
+
+Generates C++ source code for the LDPC sparse matrices for GPS L1C.
+
+José Antonio Mayo, 2026. contact@tatjam.eu
+
+-----------------------------------------------------------------------------
+
+GNSS-SDR is a Global Navigation Satellite System software-defined receiver.
+This file is part of GNSS-SDR.
+
+Copyright (C) 2022  (see AUTHORS file for a list of contributors)
+SPDX-License-Identifier: GPL-3.0-or-later
+
+----------------------------------------------------------------------------
+"""
+
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+import gps_l1c_ldpc_matrix_raw_data
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
